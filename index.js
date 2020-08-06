98e6b7b67295eea5d84986388af970d46382a76d
user-1 <user-1@MacBook-Pro.local>
2020-08-05 19:56:41 -0600

    finished step 5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
-
+const ctrl = require('./products_controller');
 const app = express();
 
 const {SERVER_PORT, CONNECTION_STRING} = process.env;
@@ -17,6 +17,12 @@ massive({
 }).catch(err => console.log(err));
 
 app.use(express.json());
+
+app.get('/api/products', ctrl.getAll);
+app.get('/api/products/:id', ctrl.getOne);
+app.put('/api/products/:id', ctrl.update);
+app.post('/api/products', ctrl.create);
+app.delete('/api/products/:id', ctrl.delete);
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server listening on port ${SERVER_PORT}.`)
